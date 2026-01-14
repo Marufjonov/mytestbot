64902379b74c0f185b13960233471cfd5ae4eed1
krasafchik prosta <krasafchik1717@mail.ru>
2026-01-14 20:26:03 +0500

    tozalanyapti

--- states/states.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class UserState(StatesGroup):
-    wait_photo = State()  # Matn ajratish uchun rasm kutish holati
-    wait_pdf = State()    # PDF kutish holati
-    wait_check = State()  # To'lov chekini kutish holati
